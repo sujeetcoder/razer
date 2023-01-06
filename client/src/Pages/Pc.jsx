85d2f5afbd69da74d52d58e04f9cd757e55d81df
Moumita Das <dasmoumita.me@gmail.com>
2023-01-06 22:58:50 +0530

    adding update to pc page half done working on

--- client/src/Pages/Pc.jsx
+++ client/src/Pages/Pc.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MetaData from '../Components/Home/MetaData'
 import PcHeader from '../Components/Pc/PcHeader'
+import PcMain from '../Components/Pc/PcMain'
 
 const Pc = () => {
   return (
   <>
   <MetaData title="Razer PC | Laptops, Desktops, and PC Peripherals ✔️"/>
       <PcHeader />
+      <PcMain/>
   </>
   )
 }
